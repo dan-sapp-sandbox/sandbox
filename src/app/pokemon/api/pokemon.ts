784f47a594036aa9d998ecacb2135bfe388d597c
Dan Sapp <dan.sapp@storable.com>
2024-10-22 17:27:11 -0500

    troubleshoot with static data

--- src/app/pokemon/api/pokemon.ts
+++ src/app/pokemon/api/pokemon.ts
@@ -1,12 +1,13 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { PokemonTable } from "@/drizzle/schema";
-import { lte } from "drizzle-orm";
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { PokemonTable } from "@/drizzle/schema";
+// import { lte } from "drizzle-orm";
+import TestData from './testData.json';
 
 export async function getPokemon() {
-  const db = drizzle(process.env.DATABASE_URL!);
-  const data = await db.select().from(PokemonTable).where(
-    lte(PokemonTable.pokedexId, 151),
-  )
-    .execute();
-  return data;
+  // const db = drizzle(process.env.DATABASE_URL!);
+  // const data = await db.select().from(PokemonTable).where(
+  //   lte(PokemonTable.pokedexId, 151),
+  // )
+  //   .execute();
+  return TestData;
 }
