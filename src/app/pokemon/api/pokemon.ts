6d840d6d8e9133965f3075e5c600843082495c78
Dan Sapp <dan.sapp@storable.com>
2024-10-22 17:11:40 -0500

    remove use server

--- src/app/pokemon/api/pokemon.ts
+++ src/app/pokemon/api/pokemon.ts
@@ -1,8 +1,6 @@
-"use server";
 import { drizzle } from "drizzle-orm/neon-http";
 import { PokemonTable } from "@/drizzle/schema";
 import { lte } from "drizzle-orm";
-import "use-server"
 
 export async function getPokemon() {
   const db = drizzle(process.env.DATABASE_URL!);
