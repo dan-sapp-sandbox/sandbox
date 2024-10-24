7677a52d35f2bee2421dd4966632baaec9bafb37
Dan Sapp <dan.sapp@storable.com>
2024-10-23 21:45:41 -0500

    linting

--- src/app/pokemon/api/pokemon.ts
+++ src/app/pokemon/api/pokemon.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { PokemonTable } from "@/drizzle/schema";
-import { lte } from "drizzle-orm";
+// import { PokemonTable } from "@/drizzle/schema";
+// import { lte } from "drizzle-orm";
 import TestData from './testData.json';
 
 export async function getPokemon() {
