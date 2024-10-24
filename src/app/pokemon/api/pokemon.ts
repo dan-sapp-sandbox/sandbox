872caf65fe3beb3c6cadc21f523f6b15b21abc27
Dan Sapp <dan.sapp@storable.com>
2024-10-23 21:35:17 -0500

    remove logging, try db

--- src/app/pokemon/api/pokemon.ts
+++ src/app/pokemon/api/pokemon.ts
@@ -1,14 +1,13 @@
-// import { drizzle } from "drizzle-orm/neon-http";
-// import { PokemonTable } from "@/drizzle/schema";
-// import { lte } from "drizzle-orm";
-import TestData from './testData.json';
+import { drizzle } from "drizzle-orm/neon-http";
+import { PokemonTable } from "@/drizzle/schema";
+import { lte } from "drizzle-orm";
+// import TestData from './testData.json';
 
 export async function getPokemon() {
-  console.log('env', process.env.DATABASE_URL);
-  // const db = drizzle(process.env.DATABASE_URL!);
-  // const data = await db.select().from(PokemonTable).where(
-  //   lte(PokemonTable.pokedexId, 151),
-  // )
-  //   .execute();
-  return TestData;
+  const db = drizzle(process.env.DATABASE_URL!);
+  const data = await db.select().from(PokemonTable).where(
+    lte(PokemonTable.pokedexId, 151),
+  )
+    .execute();
+  return data;
 }
