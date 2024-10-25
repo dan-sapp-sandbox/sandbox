14325694eda8e7516e88a68ff916fe0691971c2d
Dan Sapp <dan.sapp@storable.com>
2024-10-24 22:23:56 -0500

    restore data

--- src/app/pokemon/api/pokemon.ts
+++ src/app/pokemon/api/pokemon.ts
@@ -1,20 +1,16 @@
-import { drizzle } from "drizzle-orm/neon-http";
+// import { drizzle } from "drizzle-orm/neon-http";
 // import { PokemonTable } from "@/drizzle/schema";
 // import { lte } from "drizzle-orm";
 import TestData from "./testData.json";
-import { config } from "dotenv";
+// import { config } from "dotenv";
 
-config({ path: ".env" });
+// config({ path: ".env" });
 
 export async function getPokemon() {
-  if (!process.env.NEXT_PUBLIC_DATABASE_URL) {
-    // throw new Error("NEXT_PUBLIC_DATABASE_URL is not set");
-    return [TestData[0]];
-  }
-  const db = drizzle(process.env.NEXT_PUBLIC_DATABASE_URL!);
+  // const db = drizzle(process.env.NEXT_PUBLIC_DATABASE_URL!);
   // const data = await db.select().from(PokemonTable).where(
   //   lte(PokemonTable.pokedexId, 151),
   // )
   //   .execute();
-  return !!db ? [TestData[1]] : [TestData[2]];
+  return TestData;
 }
