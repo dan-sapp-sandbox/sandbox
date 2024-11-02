e52b2cfce63779cb9af52c38d199b6697e83b91e
Dan Sapp <dan.sapp@storable.com>
2024-11-01 21:33:39 -0500

    update url

--- src/app/pokemon/api/pokemon.ts
+++ src/app/pokemon/api/pokemon.ts
@@ -1,19 +1,7 @@
-// import { drizzle } from "drizzle-orm/neon-http";
-// import { PokemonTable } from "@/drizzle/schema";
-// import { lte } from "drizzle-orm";
 import axios from "axios";
-// import { config } from "dotenv";
-
-// config({ path: ".env" });
 
 export async function getPokemon() {
-  // const db = drizzle(process.env.NEXT_PUBLIC_DATABASE_URL!);
-  // const data = await db.select().from(PokemonTable).where(
-  //   lte(PokemonTable.pokedexId, 151),
-  // )
-  //   .execute();
-  // return axios.get("https://node-server-seven-chi.vercel.app/").then((res) =>
-  //   res.data
-  // );
-  return axios.get("http://localhost:5000/pokemon").then((res) => res.data);
+  const url = "https://node-server-seven-chi.vercel.app/pokemon";
+  // const url = "http://localhost:5000/pokemon";
+  return axios.get(url).then((res) => res.data);
 }
