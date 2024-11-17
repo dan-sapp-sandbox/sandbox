6f06b7ea0c0e7ef99fc647043e7c5fa164f8139a
Dan Sapp <dan.sapp@storable.com>
2024-11-16 19:52:42 -0600

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Portfolio() {
   return (
     <>
       <Background />
-      <div className="max-w-6xl mx-auto min-w-80 z-10 relative pt-4 lg:pt-24">
+      <div className="max-w-6xl mx-auto min-w-80 z-10 relative pt-2 lg:pt-24">
         <main className="gap-2 grid grid-flow-row grid-cols-12">
           <div className="col-span-12">
-            <div className="grid grid-cols-12 p-8">
+            <div className="grid grid-cols-12 p-4 md:p-8">
               <ProfileCard />
               <LogoList />
             </div>
