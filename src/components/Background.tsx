cb198f66b9ac8d04caab156d4b182c3a963f2b6e
Dan Sapp <dan.sapp@storable.com>
2024-11-17 17:26:36 -0600

    background

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,22 +1,8 @@
 import "./Background.css";
 export default function Background() {
-  
   return (
     <div className="background">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+      {Array.from(Array(21).keys()).map(() => <span></span>)}
     </div>
   );
 }
