b62d85b9020b24bd715479d92e23c04876643b1c
Dan Sapp <dan.sapp@storable.com>
2024-11-18 13:48:42 -0600

    lint

--- src/app/projects/pokemon/layout.tsx
+++ src/app/projects/pokemon/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { ReactNode, Suspense } from "react";
+import { ReactNode } from "react";
 import ReactQueryProvider from "./components/QueryProvider";
 
 export const metadata: Metadata = {
