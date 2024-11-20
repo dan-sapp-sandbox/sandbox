fd3ca7ea540769fb2a206a324d5969175079da23
Dan Sapp <dan.sapp@storable.com>
2024-11-20 17:57:26 -0600

    styles

--- src/app/projects/trivia/components/landing.tsx
+++ src/app/projects/trivia/components/landing.tsx
@@ -16,7 +16,7 @@ const LandingPage = (
     ? <Prompt triviaList={filteredList} />
     : (
       <div className="justify-center items-center grid grid-flow-row grid-cols-12">
-        <div className="my-2 md:my-8col-span-12 justify-center text-center">
+        <div className="my-2 md:my-8 col-span-12 justify-center text-center">
           <span className="font-sans text-2xl md:text-5xl font-bold justify-center text-center text-zinc-300">
             Horseshoes and Hand Grenades
             </span>
@@ -42,7 +42,7 @@ const LandingPage = (
         {tags.map((tag) => (
           <div key={tag} className="col-span-12 justify-center">
             <button
-              className="my-5 rounded-lg flex mx-auto bg-blue-300 hover:bg-blue-200"
+              className="my-1 md:my-5 rounded-lg flex mx-auto bg-blue-300 hover:bg-blue-200"
               onClick={async () => {
                 setFilteredList(triviaList.filter((x) => x.tag === tag));
                 setLoggedIn(true);
