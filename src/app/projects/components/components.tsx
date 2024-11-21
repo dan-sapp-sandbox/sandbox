354e00eff04e5d8b6edddf79dd5d02c69096d7bf
Dan Sapp <dan.sapp@storable.com>
2024-11-20 21:02:27 -0600

    styling

--- src/app/projects/components/components.tsx
+++ src/app/projects/components/components.tsx
@@ -20,14 +20,14 @@ export const StyledCard = (
   { btns, title, description, screenshot, logos }: iCard,
 ): JSX.Element => {
   return (
-    <div className="col-span-12 rounded bg-slate-300 my-8 p-6 z-10 relative">
+    <div className="col-span-12 rounded bg-black my-8 p-6 z-10 relative">
       <div className="grid-cols-12 grid justify-between">
         <div className="col-span-12 md:col-span-7 grid grid-cols-12">
           <div className="col-span-12 mb-2">
-            <div className="font-bold text-xl sm:text-2xl lg:text-4xl">
+            <div className="font-bold text-xl sm:text-2xl lg:text-4xl text-zinc-200">
               {title}
             </div>
-            <div className="text-md md:text-xl italic mb-2">
+            <div className="text-md md:text-xl italic mb-2 text-zinc-200">
               {description}
             </div>
             <div className="flex align-middle flex-wrap">
@@ -53,7 +53,7 @@ export const StyledCard = (
           {/* eslint-disable-next-line */}
           <img
             alt="pokemon-screen-shot"
-            className="max-h-80 mb-4 md:mb-0 border-black border-2 rounded-md hover:border-blue-400"
+            className="max-h-80 mb-4 md:mb-0 border-zinc-300 border-2 rounded-md hover:border-blue-400"
             src={screenshot.imageUrl}
           />
         </Link>
