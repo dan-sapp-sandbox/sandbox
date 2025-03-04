4d14c58d093cefcebd937a616c88f802a25763d1
Dan Sapp <dan.sapp@storable.com>
2025-03-03 18:29:37 -0600

    update links after deploy

--- src/app/projects/containers/FlappyBirdCard.tsx
+++ src/app/projects/containers/FlappyBirdCard.tsx
@@ -5,18 +5,17 @@ const FlappyBirdData: iCard = {
   description: "Flappy Bird written in typescript.",
   screenshot: {
     imageUrl: "/static/images/flappy-bird-screenshot.png",
-    linkUrl: "/projects/pages/calculator",
+    linkUrl: "https://flappy-bird-five-delta.vercel.app/",
   },
   btns: [
     {
       text: "App Code",
-      url:
-        "https://github.com/dan-sapp-sandbox/sandbox/tree/main/src/app/projects/pages/flappy-bird",
+      url: "https://github.com/dan-sapp-sandbox/flappy-bird",
       icon: "/static/images/github.svg",
     },
     {
       text: "Try It!",
-      url: "/projects/pages/calculator",
+      url: "https://flappy-bird-five-delta.vercel.app/",
     },
   ],
 };
