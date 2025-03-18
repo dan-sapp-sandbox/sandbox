fd5a02b4ea26e44a14312cca56f21026cf956448
Dan Sapp <dan.sapp@storable.com>
2025-03-17 19:35:26 -0500

    Update DashboardCard.tsx

--- src/app/sandbox/cards/DashboardCard.tsx
+++ src/app/sandbox/cards/DashboardCard.tsx
@@ -11,7 +11,7 @@ const DashboardData: iCard = {
     {
       text: "App Code",
       url:
-        "https://github.com/dan-sapp-sandbox/sandbox/tree/main/src/app/projects/pages/dashboard",
+        "https://github.com/dan-sapp-sandbox/dashboard",
       icon: "/static/images/logos/github.svg",
     },
     {
