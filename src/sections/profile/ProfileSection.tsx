6f234b15f10e8aa8f32b67390ba3822410a7314b
Dan Sapp <dan.sapp@storable.com>
2026-02-09 12:36:19 -0600

    favicon and personal links

--- src/sections/profile/ProfileSection.tsx
+++ src/sections/profile/ProfileSection.tsx
@@ -18,12 +18,28 @@ const ProfileSection = () => {
             <span>I'm a software engineer with 10 years of industry experience.</span>
             <span>I like making cool stuff that feels good to use.</span>
             <span>I have degrees in Physics/Math and Biology/Business.</span>
-            <div
-              onClick={profileState.handleOpenResume}
-              className="flex flex-row items-center gap-2 cursor-pointer text-blue-400 hover:text-blue-300"
-            >
-              <span>Link to resume</span>
-              <ExternalLink className="size-5" />
+            <div className="flex flex-row items-center gap-16">
+              <div
+                onClick={profileState.handleOpenResume}
+                className="flex flex-row items-center gap-2 cursor-pointer text-blue-400 hover:text-blue-300"
+              >
+                <span>Resume</span>
+                <ExternalLink className="size-5" />
+              </div>
+              <div
+                onClick={profileState.handleOpenLinkedInLink}
+                className="flex flex-row items-center gap-2 cursor-pointer text-blue-400 hover:text-blue-300"
+              >
+                <span>LinkedIn</span>
+                <ExternalLink className="size-5" />
+              </div>
+              <div
+                onClick={profileState.handleOpenGithubLink}
+                className="flex flex-row items-center gap-2 cursor-pointer text-blue-400 hover:text-blue-300"
+              >
+                <span>GitHub</span>
+                <ExternalLink className="size-5" />
+              </div>
             </div>
           </div>
         </div>
--- src/sections/profile/useProfileState.tsx
+++ src/sections/profile/useProfileState.tsx
@@ -1,10 +1,20 @@
 export const useProfileState = () => {
+  const githubURL = "https://github.com/dan-sapp-sandbox";
+  const linkedInURL = "https://www.linkedin.com/in/dan-sapp-744145b6/";
   const resumeURL =
     "https://docs.google.com/document/d/e/2PACX-1vTxM5BOxqBemhI7wWHlvLhdaQ13Qbuw0GONkrww9KVQ3HAsuBHEwrHJuipAKfF5Tg/pub";
+  const handleOpenGithubLink = () => {
+    window.open(githubURL, "_blank", "noopener,noreferrer");
+  };
+  const handleOpenLinkedInLink = () => {
+    window.open(linkedInURL, "_blank", "noopener,noreferrer");
+  };
   const handleOpenResume = () => {
     window.open(resumeURL, "_blank", "noopener,noreferrer");
   };
   return {
+    handleOpenGithubLink,
+    handleOpenLinkedInLink,
     handleOpenResume,
   };
 };
