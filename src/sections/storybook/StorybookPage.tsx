2c9cfa7a35066cc84fe3624ee47165520125f27d
Dan Sapp <dan.sapp@storable.com>
2026-02-13 18:38:09 -0600

    try other build

--- src/sections/storybook/StorybookPage.tsx
+++ src/sections/storybook/StorybookPage.tsx
@@ -2,7 +2,7 @@ export default function StorybookPage() {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
-        src="../storybook-static/index.html"
+        src="/storybook/index.html"
         title="Storybook"
         style={{
           width: "100%",
