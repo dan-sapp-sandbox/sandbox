f1ffc888815e351644c5c3d8ddaba838654cd2a7
Dan Sapp <dan.sapp@storable.com>
2026-02-27 17:11:08 -0600

    pip movable

--- src/sections/map/PipMap.tsx
+++ src/sections/map/PipMap.tsx
@@ -1,8 +1,11 @@
 import { useContext, useEffect, useMemo } from "react";
 import type { JSX } from "react";
+import { Grip } from "lucide-react";
 import { Viewer, useCesium } from "resium";
 import { CameraContext, type IWidget } from "./types";
 import { Cartesian3 } from "cesium";
+import { useDraggable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
 
 const PipInitializer = () => {
   const { viewer } = useCesium();
@@ -25,6 +28,9 @@ const PipInitializer = () => {
 };
 
 const PipMap = ({ children, pipState }: { children?: JSX.Element | JSX.Element[]; pipState: IWidget }) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: "pip",
+  });
   const contextOptions = useMemo(() => ({ webgl: { alpha: true } }), []);
   return (
     <div
@@ -33,26 +39,41 @@ const PipMap = ({ children, pipState }: { children?: JSX.Element | JSX.Element[]
         left: `${pipState.left}%`,
         width: `${pipState.width}%`,
         aspectRatio: pipState.aspect,
+        transform: CSS.Translate.toString(transform),
       }}
-      className="absolute rounded border border-(--pip-border) overflow-hidden"
+      className="group absolute rounded border border-(--pip-border) overflow-hidden"
+      ref={setNodeRef}
     >
-      <Viewer
-        full
-        contextOptions={contextOptions}
-        baseLayerPicker={false}
-        timeline={false}
-        geocoder={false}
-        homeButton={false}
-        sceneModePicker={false}
-        animation={false}
-        fullscreenButton={false}
-        navigationHelpButton={false}
-        selectionIndicator={false}
-        infoBox={false}
-      >
-        <PipInitializer />
-        {children}
-      </Viewer>
+      <div className="z-999 w-full h-full relative">
+        <div className="absolute top-0 w-full flex flex-row justify-center">
+          <div
+            className="cursor-grab active:cursor-grabbing opacity-0 group-hover:opacity-100 transition rounded bg-black/70 p-1"
+            {...listeners}
+            {...attributes}
+          >
+            <Grip className="h-5 w-5 stroke-white" />
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-0 left-0 h-full w-full">
+        <Viewer
+          full
+          contextOptions={contextOptions}
+          baseLayerPicker={false}
+          timeline={false}
+          geocoder={false}
+          homeButton={false}
+          sceneModePicker={false}
+          animation={false}
+          fullscreenButton={false}
+          navigationHelpButton={false}
+          selectionIndicator={false}
+          infoBox={false}
+        >
+          <PipInitializer />
+          {children}
+        </Viewer>
+      </div>
     </div>
   );
 };
