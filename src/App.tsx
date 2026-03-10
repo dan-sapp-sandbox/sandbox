36ff3b964fddfc5b42fc780c73fae47f5e655ea5
Dan Sapp <dan.sapp@storable.com>
2026-03-09 23:20:52 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,7 @@ import Sections from "./sections";
 
 const App = () => {
   return (
-    <div
-      style={{
-        // backgroundImage: "url(/background.jpg)",
-        // backgroundRepeat: "no-repeat",
-        // backgroundSize: "cover",
-        // background: `linear-gradient(
-        //   135deg,
-        //   #0f172a 0%,
-        //   #0b1f3a 40%,
-        //   #020617 100%
-        // )`,
-        background: `radial-gradient(
-            circle at 20% 20%,
-            #1e3a8a22,
-            transparent 40%
-          ),
-          radial-gradient(
-            circle at 80% 80%,
-            #2563eb22,
-            transparent 40%
-          ),
-          #020617`,
-      }}
-      className="h-full w-full flex flex-col gap-4 md:gap-6 overflow-y-scroll scrollbar-hide"
-    >
+    <div className="h-full w-full flex flex-col gap-4 md:gap-6 overflow-y-scroll scrollbar-hide bg-linear-to-br from-slate-900 via-blue-950 to-black">
       <div className="w-full p-4 md:p-8 flex flex-row justify-center">
         <div className="w-full max-w-400 flex flex-col justify-center items-center gap-4 md:gap-8 overflow-hidden">
           <Sections.Profile />
