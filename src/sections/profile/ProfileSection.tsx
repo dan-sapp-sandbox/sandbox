2c10a2cdf97b6252119550858f748532a2431fc9
Dan Sapp <dan.sapp@storable.com>
2026-03-17 13:22:27 -0500

    cleanup

--- src/sections/profile/ProfileSection.tsx
+++ src/sections/profile/ProfileSection.tsx
@@ -3,7 +3,7 @@ const ProfileSection = () => {
     <div className="w-full px-2 py-12 md:py-24 lg:py-32 flex flex-row justify-center items-center bg-linear-to-b from-(--profile-bg-1) via-(--profile-bg-2) to-(--profile-bg-3)">
       <div className="max-w-400">
         <div className="w-full flex flex-row gap-12">
-          <img src="/me2.png" className="h-16 md:h-28 rounded-[120px]" alt="profile pic" />
+          <img src="/me.png" className="h-16 md:h-28 rounded-[120px]" alt="profile pic" />
           <div className="h-full flex-1 flex flex-col justify-between overflow-hidden gap-4 rounded-2xl">
             <div className="flex flex-col gap-3 md:gap-8">
               <span className="text-xl md:text-4xl font-bold text-(--text)">Hi, I'm Dan Sapp.</span>
