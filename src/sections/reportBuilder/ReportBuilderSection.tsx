a63f56a927a990cccb34139441c42ed5a06fbf4f
Dan Sapp <dan.sapp@storable.com>
2026-03-18 19:40:53 -0500

    Update ReportBuilderSection.tsx

--- src/sections/reportBuilder/ReportBuilderSection.tsx
+++ src/sections/reportBuilder/ReportBuilderSection.tsx
@@ -6,7 +6,7 @@ const ReportBuilderSection = () => {
     githubURL: "https://github.com/dan-sapp-sandbox/sandbox/tree/main/src/sections/reportBuilder",
     demoURL: "/report-builder",
     title: "Report Builder",
-    description: "Used in conjuction with other tools to generate a customizable report with a live pdf preview.",
+    description: "Used in conjunction with other tools to generate a customizable report with a live pdf preview.",
     features: [
       "Expand Sections to use Rich Text Editor",
       "Sections Drag & Drop: Reorder sections",
